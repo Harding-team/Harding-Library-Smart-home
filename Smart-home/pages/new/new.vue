1264d75c622ee89e3aeb1c6a2cfb46dfd9d2e2bf
HardingDuan <dhq707181451@163.com>
2019-11-25 10:16:44 +0800

    duan1

---
+++ Smart-home/pages/new/new.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello2'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>

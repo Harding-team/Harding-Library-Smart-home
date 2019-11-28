98a2846ead6f2af96cdbfd1474e1ca3d4ebf4c26
qiwenzheng <361405342@qq.com>
2019-11-28 10:25:15 +0800

    qi11

--- Smart-home/pages/coupon/coupon.vue
+++ Smart-home/pages/coupon/coupon.vue
@@ -36,7 +36,6 @@
 					<view>收藏+关注</view>
 				</view>
 				<view class="btn">
-					<view>立即使用</view>
 					<view  @click="goToDetails(item.id)">立即使用</view>
 				</view>
 			</view>
@@ -55,12 +54,6 @@
 				number:0,
 				couponArr:[
 					{
-						price:'5',
-						manPrice:'199',
-					},{
-						price:'55',
-						manPrice:'500',
-					},{
 						price:'2',
 						manPrice:'100',
 						id:'1'
@@ -69,8 +62,8 @@
 						manPrice:'500',
 						id:'2'
 					},{
-						price:'2',
-						manPrice:'100',
+						price:'245',
+						manPrice:'20',
 						id:'3'
 					}
 				]
--- Smart-home/unpackage/dist/dev/mp-weixin/common/vendor.js
+++ Smart-home/unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -7501,9 +7501,9 @@ module.exports = g;
 
 /***/ }),
 /* 4 */
-/*!***************************************************************!*\
-  !*** D:/git/Harding-Library-Smart-home/Smart-home/pages.json ***!
-  \***************************************************************/
+/*!*****************************************************************************************!*\
+  !*** C:/Users/齐文蒸/Desktop/智能家具uni-app/Harding-Library-Smart-home/Smart-home/pages.json ***!
+  \*****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8408,9 +8408,9 @@ module.exports = {"_from":"@dcloudio/uni-stat@next","_id":"@dcloudio/uni-stat@2.
 
 /***/ }),
 /* 7 */
-/*!********************************************************************************!*\
-  !*** D:/git/Harding-Library-Smart-home/Smart-home/pages.json?{"type":"style"} ***!
-  \********************************************************************************/
+/*!**********************************************************************************************************!*\
+  !*** C:/Users/齐文蒸/Desktop/智能家具uni-app/Harding-Library-Smart-home/Smart-home/pages.json?{"type":"style"} ***!
+  \**********************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8419,9 +8419,9 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 
 /***/ }),
 /* 8 */
-/*!*******************************************************************************!*\
-  !*** D:/git/Harding-Library-Smart-home/Smart-home/pages.json?{"type":"stat"} ***!
-  \*******************************************************************************/
+/*!*********************************************************************************************************!*\
+  !*** C:/Users/齐文蒸/Desktop/智能家具uni-app/Harding-Library-Smart-home/Smart-home/pages.json?{"type":"stat"} ***!
+  \*********************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
